7178c748b3db2eb04ce38536e088563d4fe834c7
Maati <douwah95@gmail.com>
2020-05-02 11:28:16 +0000

    commit

---
+++ src/test/java/com/mycompany/stock/GestionStockV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStockV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
